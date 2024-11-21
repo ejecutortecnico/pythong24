6d15765e4cc9d54b2f4bd282e08f0142e084cb4c
ejecutortecnico <ejecutortecnico@gmail.com>
2024-11-21 17:11:40 -0500

    test

--- pruebapandas.py
+++ pruebapandas.py
@@ -1,11 +1,19 @@
 import numpy as np
 import pandas as pd
 
-# Crear una Serie
-serie = pd.Series([1, 2, 3, 4, 5])
-print(serie)
-
-# Crear un DataFrame
+# Leer un archivo CSV
+#df = pd.read_csv("poblacion.csv")
 datos = {"Nombre": ["Ana", "Juan", "Pedro"], "Edad": [25, 30, 35]}
-df = pd.DataFrame(datos)
+df = pd.DataFrame(datos)       # Primeras 5 filas
 print(df)
+# Agregar una nueva columna
+df["Salario"] = [3000, 4000, 5000]
+print(df)
+# Modificar valores
+df["Edad"] = df["Edad"] + 1
+print(df)
+
+# Eliminar una columna
+df = df.drop(0, axis=0)
+print(df)
+            
