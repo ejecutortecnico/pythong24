62022084d6ceeba3f33b975ca19cfdf9b23de797
ejecutortecnico <ejecutortecnico@gmail.com>
2024-11-21 15:38:59 -0500

    test

--- pruebanumpy.py
+++ pruebanumpy.py
@@ -10,7 +10,7 @@ import numpy as np
 # arr_2d = np.array(lista2)
 # print(arr_2d)
 lista1=[1, 2, 3,5,6,7,8,9]
-lista2=[4, 5, 6,8,9,5,3,5]
+lista2=[4, 5, 6,8,9,5,6,5]
 resultado=[]
 for i in range(len(lista1)):
     resultado.append(lista1[i]+lista2[i])
@@ -21,3 +21,9 @@ arr2 = np.array(lista2)
 # Suma de arrays
 resultado = np.add(arr1, arr2)
 print(resultado)
+media = np.mean(arr2)
+mediana = np.median(arr2)
+ds = np.std(arr2)
+print(media)
+print(mediana)
+print(ds)
